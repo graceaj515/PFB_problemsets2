9af9a825af7072e991fef04f90994a3ef4e284aa
student <student@info-12.cshl.edu>
2022-10-14 11:46:24 -0400

    python 7 problems

---
+++ python7/3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/evn python3
+import re
+with open('Python_07.fasta', 'r') as fasta_file:
+	for line in fasta_file:
+		line = line.rstrip()
+		fastas = re.match(r">(\S+)\s(.*)", line)
+		if fastas:
+			print("Extracted seq name:", fastas.group(1), "Extracted description:", fastas.group(2))
+		else:
+			pass
